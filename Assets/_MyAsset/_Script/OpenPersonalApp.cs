b652468c6aa20b0e499c940dec8e399b9795b95d
acastor <anthony@globalmobility.net>
2021-01-18 15:46:53 +0800

    Asset

---
+++ Assets/_MyAsset/_Script/OpenPersonalApp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AppLauncherPlugin;
+
+public class OpenPersonalApp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MobileAppLauncher(string appID)
+    {
+        AppLauncher.LaunchApp(""+ appID, gameObject.name);
+        print("Launch: " + appID);
+    }
+}
