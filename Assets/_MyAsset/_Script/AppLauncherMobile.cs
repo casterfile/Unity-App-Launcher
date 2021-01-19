1146a1935507f624af3c4da48fbcbc55d12bdcca
acastor <anthony@globalmobility.net>
2021-01-19 09:34:52 +0800

    Asset2

---
+++ Assets/_MyAsset/_Script/AppLauncherMobile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AppLauncherPlugin;
+
+public class AppLauncherMobile : MonoBehaviour
+{
+    public string AppID;//com.mobilemobility.agec sample name
+    public void MobileAppLauncher()
+    {
+        AppLauncher.LaunchApp(""+ AppID, gameObject.name);
+        print("Launch: " + AppID);
+    }
+}
--- Assets/_MyAsset/_Script/OpenPersonalApp.cs
+++ Assets/_MyAsset/_Script/OpenPersonalApp.cs
@@ -5,17 +5,6 @@ using AppLauncherPlugin;
 
 public class OpenPersonalApp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void MobileAppLauncher(string appID)
     {
